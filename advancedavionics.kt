5db4a6ea39c963de4e5ddc6198d81183fbc1f048
touristy <44515154+touristy@users.noreply.github.com>
2018-10-27 07:38:59 +0530

    Create advancedavionics.kt

---
+++ advancedavionics.kt
@@ -0,0 +1,4 @@
+Kotlin is new, but although there isn't much stuff on Stack over flow for it, I'm already using it for avionics of my new drony.
+
+class drony:dronySuperClass 
+    printing stmt synt in kotlin
